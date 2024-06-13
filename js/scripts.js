872bf67c02eeda6054163cfe44b0d37bba5db1b1
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-13 19:52:53 +0700

    Add styles to error match message

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,24 @@ function passwordValidation() {
   passwordInputConf.addEventListener('input', () => {
     if (passwordInput.value !== passwordInputConf.value) {
       errorMsg.classList.add('show');
+      passwordInput.style.outline = '1px solid red';
+      passwordInputConf.style.outline = '1px solid red';
     } else {
       errorMsg.classList.remove('show');
+      passwordInput.removeAttribute('style');
+      passwordInputConf.removeAttribute('style');
     }
   })
 
   passwordInput.addEventListener('input', () => {
     if (passwordInput.value !== passwordInputConf.value) {
       errorMsg.classList.add('show');
+      passwordInput.style.outline = '1px solid red';
+      passwordInputConf.style.outline = '1px solid red';
     } else {
       errorMsg.classList.remove('show');
+      passwordInput.removeAttribute('style');
+      passwordInputConf.removeAttribute('style');
     }
   })
 }
